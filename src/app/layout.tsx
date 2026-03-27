2a008b264cd1d68214df80ca2692ef3db340d1fc
jefferyleo <jefferyleo93@hotmail.com>
2026-03-28 00:44:20 +0800

    Update GA

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,10 +28,10 @@ export default function RootLayout({
             gtag('config', 'G-ZNHZZFTQK5');
           `}
         </Script>
-        <Script
+        <script
+          async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8484071240501962"
           crossOrigin="anonymous"
-          strategy="afterInteractive"
         />
       </head>
       <body className="bg-gray-950 text-gray-100 min-h-screen">
